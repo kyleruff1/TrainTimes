e2979765b9dcc670664ed514bae105ac5a062fdb
Kyle Ruff <kyleruff@gmail.com>
2019-06-26 17:57:02 -0600

    styling inside of html

--- game.js
+++ game.js
@@ -117,36 +117,5 @@ var config = {
       )
     );
   });
-  
-  // Assume the following situations.
-  
-  // (TEST 1)
-  // First Train of the Day is 3:00 AM
-  // Assume Train comes every 3 minutes.
-  // Assume the current time is 3:16 AM....
-  // What time would the next train be...? ( Let's use our brains first)
-  // It would be 3:18 -- 2 minutes away
-  
-  // (TEST 2)
-  // First Train of the Day is 3:00 AM
-  // Assume Train comes every 7 minutes.
-  // Assume the current time is 3:16 AM....
-  // What time would the next train be...? (Let's use our brains first)
-  // It would be 3:21 -- 5 minutes away
-  
-  // ==========================================================
-  
-  // Solved Mathematically
-  // Test case 1:
-  // 16 - 00 = 16
-  // 16 % 3 = 1 (Modulus is the remainder)
-  // 3 - 1 = 2 minutes away
-  // 2 + 3:16 = 3:18
-  
-  // Solved Mathematically
-  // Test case 2:
-  // 16 - 00 = 16
-  // 16 % 7 = 2 (Modulus is the remainder)
-  // 7 - 2 = 5 minutes away
-  // 5 + 3:16 = 3:21
+
   
